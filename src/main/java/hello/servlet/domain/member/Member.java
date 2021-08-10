c0152d4b6de4de88befb12b7db202b59845d6347
younsikMin <minys94@daum.net>
2021-08-10 17:32:57 +0900

    FrontController v5-v4

--- src/main/java/hello/servlet/domain/member/Member.java
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -1,9 +1,10 @@
 package hello.servlet.domain.member;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
+
+
+
+
 public class Member {
 
     private Long id;
@@ -15,6 +16,30 @@ public class Member {
 
     public Member(String username, int age) {
         this.username = username;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
